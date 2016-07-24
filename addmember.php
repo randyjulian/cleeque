7ab229bbe45a7a3c3149341cdb88d6d4626139f8
randyjulian <randyjulian.ykl@gmail.com>
2016-07-24 22:14:03 +0800

    chnaged the meeting background

--- addmember.php
+++ addmember.php
@@ -21,7 +21,7 @@ $_SESSION['groupID']=$_POST['groupNameSelected'];
 <body>
 
 <div class="navbar">
-		<img id="logo" src="http://i.imgur.com/NXXGa4e.png" height="35" width="35" style="float: left; margin-top: 6.4px;"><p id= "cleeque" style="margin-top:0px;" >  CLEEQUE</p> 
+		<img id="logo" src="http://i.imgur.com/H4esOFn.jpg" height="35" width="35" style="float: left; margin-top: 6.4px;"><p id= "cleeque" style="margin-top:0px;" >  CLEEQUE</p> 
 		<div class="menu" style="float:right;">
 			<div class="mainMenu">
 				<p >Home</p>
