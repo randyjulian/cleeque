9cda649ca3c76d2ac640162422a6391f0aa1dc30
randyjulian <randyjulian.ykl@gmail.com>
2016-07-24 14:29:57 +0800

    make logo in navbar smaller and added photo on the black rectangle in login Page

--- loginPage.php
+++ loginPage.php
@@ -40,7 +40,7 @@
 		</div>
 	</div>
 	<div class="navbar">
-		<img id="logo" src="http://i.imgur.com/NXXGa4e.png" height="45" width="45" style="float: left;"><p id= "cleeque" style="margin-top:0px;" >  CLEEQUE</p> 
+		<img id="logo" src="http://i.imgur.com/NXXGa4e.png" height="35" width="35" style="float: left; margin-top: 6.4px;"><p id= "cleeque" style="margin-top:0px;" >  CLEEQUE</p> 
 		<div class="menu" style="float:right;">
 			<div class="mainMenu">
 				<p >Home</p>
