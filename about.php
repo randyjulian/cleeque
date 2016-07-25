fe8e8572ae03ba85865e2117218793ecd64f9cc6
Krittin Kawkeeree <kitsada280@gmail.com>
2016-07-25 13:31:43 +0800

    About page

---
+++ about.php
@@ -0,0 +1,79 @@
+<?php
+	session_start();
+	include("groupFunction.php");
+	include("main_ics_processer.php");
+	include("databaseconnection.php");
+	$username=$_SESSION['username'];
+	$_SESSION['fullName']=gettingNameFromUsername($_SESSION['username']);
+	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="main.js"></script>
+	<title>Cleeque | Dashboard</title>
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+	<meta name="theme-color" content="#ffffff">
+	<link type="text/css" rel="stylesheet" href="style.css"></link>
+	<!---Fonts-->
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="modal">
+		<div class="modal-content">
+			<div class="modalHeader">
+				<span class="close"> X </span>
+				<p id="modalCleeque">CLEEQUE</p>				
+			</div>
+			<div class="modalBody">
+				<form  method="post">
+					<input type="text" id="username" placeholder="Username"><br><br>
+					<input type="password" id="password" placeholder="Password"><br>
+					<p id="errorMessage" style="color:red"></p>
+					<input type="submit" name="submit" value="Sign In" id="loginButton">
+				</form>
+				
+				<br id="account"> Don't have an account? <a href="signup.php" id="modalSignup">Sign up!</a>
+			</div>
+		</div>
+	</div>
+	<div class="navbar">
+		<img id="logo" src="http://i.imgur.com/NXXGa4e.png" height="35" width="35" style="float: left; margin-top: 6.4px;"><p id= "cleeque" style="margin-top:0px;" >  CLEEQUE</p> 
+		<div class="menu" style="float:right;">
+			<div class="mainMenu">
+				<p >Home</p>
+				<p>About</p>
+				<p>Contact Us</p>
+			</div>
+			<?php 
+				if(isset($_SESSION['username'])){
+					echo '<a id="usernameNav" style="text-decoration:none;" href="logout.php">Log Out</a>';
+				} else {
+					echo '<p id="login">Sign In</p>';
+				}
+			?>
+			<a id="usernameNav" style="text-decoration:none;" href="logout.php">Log Out</a>
+			<p id="responsiveNavButton"> &#9776; Menu</p>
+		</div>
+		
+
+	</div>
+	<div class="main">
+		<div class="description">
+			<h1 id="descriptionHeader">What's this project?</h1>
+			<h2 id="descriptionContent">Cleeque aims to facilitate meet up arrangement so that work can be more productive and friends would be more bonded. This web application is designed especially for NUS students but with a big plan to extend beyond the NUS community. As such, currently, the website only supports the iCalendar file downloaded from nusmods.com.
+			<br>
+			This web application is a part of the NUS computing project module called "Orbitals". If you have any feedback or suggestion, please drop us an email at <strong>krittin.kawkeeree@u.nus.edu</strong> or <strong>RANDY EMAIL</strong>.
+		</div>
+		
+	</div>
+	<div class="footer">
+		<p style="text-align: left;"> &copy Cleeque 2016</p>
+	</div>
+
+</body>
+</html>
