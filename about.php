0e11b9858a8a329e559ca98f7d66b882f411c0a1
Krittin Kawkeeree <kitsada280@gmail.com>
2016-07-25 13:44:30 +0800

    Change h1/h2 to p

--- about.php
+++ about.php
@@ -63,11 +63,11 @@
 	</div>
 	<div class="main" style="padding-top:50px;">
 		<div class="description">
-			<h1 id="descriptionHeader">What's this project?</h1>
-			<h2 id="descriptionContent">Cleeque aims to facilitate meet up arrangement so that work can be more productive and friends would be more bonded. This web application is designed especially for NUS students but with a big plan to extend beyond the NUS community. As such, currently, the website only supports the iCalendar file downloaded from <a href="https://nusmods.com/">nusmods.com</a>.
+			<p id="descriptionHeader">What's this project?</p>
+			<p id="descriptionContent">Cleeque aims to facilitate meet up arrangement so that work can be more productive and friends would be more bonded. This web application is designed especially for NUS students but with a big plan to extend beyond the NUS community. As such, currently, the website only supports the iCalendar file downloaded from <a href="https://nusmods.com/">nusmods.com</a>.
 			<br>
 			<br>
-			This web application is a part of the NUS computing project module called "Orbitals". If you have any feedback or suggestion, please drop us an email at <strong>krittin.kawkeeree@u.nus.edu</strong> or <strong>RANDY EMAIL</strong>.
+			This web application is a part of the NUS computing project module called "Orbitals". If you have any feedback or suggestion, please drop us an email at <strong>krittin.kawkeeree@u.nus.edu</strong> or <strong>RANDY EMAIL</strong>.</p>
 		</div>
 		
 	</div>
