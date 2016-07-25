9d76fb6636b138eee5172ce08bdec5ab683d553e
randyjulian <randyjulian.ykl@gmail.com>
2016-07-25 17:21:06 +0800

    removed hidden css

--- peopledependent.php
+++ peopledependent.php
@@ -19,7 +19,7 @@
     -->
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+	
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 	<script type="text/javascript" src="main.js"></script>
 	<title>Cleeque | People Dependent Slots</title>
