b9a33d2436e48990d62c5cbe4845ec90f267439f
randyjulian <randyjulian.ykl@gmail.com>
2016-07-25 22:31:44 +0800

    no message

--- about.php
+++ about.php
@@ -62,7 +62,6 @@
 	</div>
 	<div class="main" style="padding-top:50px;">
 		<div class="description">
-		<img src="http://i.imgur.com/YmE8ECm.png" style="margin-left: auto;margin-right: auto;">
 			<p id="descriptionHeader">What's this project?</p>
 			<p id="descriptionContent">Cleeque aims to facilitate meet up arrangement so that work can be more productive and friends would be more bonded. This web application is designed especially for NUS students but with a big plan to extend beyond the NUS community. As such, currently, the website only supports the iCalendar file downloaded from <a href="https://nusmods.com/" style="text-decoration:none;">nusmods.com</a>.
 			<br>
