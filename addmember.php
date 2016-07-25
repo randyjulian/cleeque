d1d4b3bd6133370c8e3820da5882f92a37469d7b
randyjulian <randyjulian.ykl@gmail.com>
2016-07-25 22:06:55 +0800

    no message

--- addmember.php
+++ addmember.php
@@ -172,7 +172,7 @@ printTableArray($freeTimeArray);
 
 
 <form action="peopledependent.php" method='POST'>
-	<p id="selected"  style="text-align: center;">
+	<p id="selected"  style="text-align: center;"><br>
 	<input  style="margin: auto;" class="peopledependent" type='submit' name='submit' value='Find common free slot for selected users!'><br>
 </form>
 
