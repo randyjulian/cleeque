6e02960609eec2d209032ae58f35d9ac075e96e9
Krittin Kawkeeree <kitsada280@gmail.com>
2016-07-31 11:41:23 +0800

    Add font links to the webpage and add the "The group's timetable"

--- addmember.php
+++ addmember.php
@@ -43,6 +43,8 @@ if (!isset($_POST['username'])) {
 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 	<script type="text/javascript" src="main.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
@@ -116,6 +118,13 @@ if (!isset($_POST['username'])) {
     color: white;
     transition: 0.2s;
 	}
+
+	.groupTableHeader{
+		font-family: "Montserrat";
+		font-size: 20px;
+		text-align: center;
+		clear: both;
+	}
 </style>
 
 <div class="navbar">
@@ -140,6 +149,7 @@ if (!isset($_POST['username'])) {
 			</form>
 		</div>
 		<div class="showTableDiv">
+			<p class="groupTableHeader">The Group's Timetable</p>
 <?php
 //Initialise the free time array
 $groupMemberArray=gettingGroupMember($_SESSION['groupID']);
