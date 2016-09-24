5636b949eb3fd06f2fb77fc75fd301689549d797
Krittin Kawkeeree <kitsada280@gmail.com>
2016-09-24 10:56:53 +0800

    Debugging

--- debugging.php
+++ debugging.php
@@ -1,8 +1,6 @@
 <?php
 	include('databaseconnection.php');
-	$sql= "SELECT TOP(1) FROM groupmember WHERE groupID = '352' AND userID ='872' "; 
-	$stmt = $database->prepare($sql);
-	$stmt->execute();
-	$array = $stmt->fetchColumn();
-	print_r($array);
+	$sql= "DELETE FROM groupmember WHERE groupID = '352' AND userID ='872' "; 
+	$database->exec($sql);
+	header('Location: loginPage.php');
 ?>
